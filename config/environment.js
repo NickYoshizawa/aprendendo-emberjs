5b13c913abe2de192f57e04a9c6ce372c13f95e3
NickYoshizawa <nicolas.yoshizawa2005@gmail.com>
2025-07-09 12:11:22 -0300

    Arrumando o nome do projeto

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function (environment) {
   const ENV = {
-    modulePrefix: 'ember-js',
+    modulePrefix: 'aprendendo-emberjs',
     environment,
     rootURL: '/',
     locationType: 'history',
